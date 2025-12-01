e49bcaaee13708170ba8a6b5f7c797780958ab3d
Damian <damian.aebi@nexplore.ch>
2025-12-01 17:05:13 +0100

    failed attempt at second star

--- 2025/Day01/day_01.py
+++ 2025/Day01/day_01.py
@@ -1,7 +1,11 @@
+import math
+
+
 class Dial:
     def __init__(self, number):
         self._number = number
         self.number_history = [number]
+        self.passed_zero_counter = 0
 
     @property
     def number(self):
@@ -17,19 +21,25 @@ class Dial:
         direction_number = int(rotation[1:])
 
         if direction_char == 'R':
+            self.passed_zero_counter += math.floor((self.number + direction_number) / 100)
             self.number += direction_number
 
         elif direction_char == 'L':
+            if self.number - direction_number < 0:
+                self.passed_zero_counter += math.ceil(math.fabs(self.number - direction_number) / 100)
+            elif self.number - direction_number == 0:
+                self.passed_zero_counter += 1
             self.number -= direction_number
+
 
         # print(self)
 
     def __str__(self):
-        return str("Lock is at: " + str(self.number))
+        return str("Lock is at: " + str(self.number) + " 0 Counter: " + str(self.passed_zero_counter))
 
 
 dummyValues = [
-    "L68",
+    "L160",
     "L30",
     "R48",
     "L5",
@@ -42,12 +52,12 @@ dummyValues = [
 
 with open("input.txt", "r") as input_file:
     rotationValues = [line.strip() for line in input_file]
-    print(rotationValues)
 
 dial = Dial(50)
 
 for rotation in rotationValues:
+    # print("Rotating with: " + rotation)
     dial.rotate(rotation)
 
-print(dial.number_history)
 print(dial.number_history.count(0))
+print(dial.passed_zero_counter)

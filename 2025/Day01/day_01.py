ca8ab098ef16ee406a483dd8ba27f1ea7771c463
Damian <damian.aebi@nexplore.ch>
2025-12-01 14:40:36 +0100

    first working solution

--- 2025/Day01/day_01.py
+++ 2025/Day01/day_01.py
@@ -1,1 +1,47 @@
-print("hello world")
+class Dial:
+    def __init__(self, number) :
+        self._number = number
+
+    @property
+    def number(self):
+        return self._number
+
+    @number.setter
+    def number(self, value):
+        self._number = value % 99
+
+    def rotate(self, rotation) :
+        direction_char = rotation[:1]
+        direction_number = int(rotation[1:])
+
+        if direction_char == 'R':
+            self.number += direction_number
+
+        elif direction_char == 'L':
+            self.number -= direction_number
+
+        print(self)
+
+    def __str__(self):
+        return str("Lock is at: " + str(self.number))
+
+
+dummyValues = [
+    "R13",
+    "L5",
+    "L3",
+    "R5",
+    "L2",
+    "R2",
+    "L1",
+    "R1",
+    "R120",
+    "R423",
+    "L70",
+    "R4"
+]
+
+dial = Dial(50)
+
+for rotation in dummyValues:
+    dial.rotate(rotation)

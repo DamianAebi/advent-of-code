3455608b33a51a4c8334e73d4b31bfc59c442d68
Damian <damian.aebi@nexplore.ch>
2025-12-02 11:10:44 +0100

    Day2, first star successful

--- 2025/Day02/day_02.py
+++ 2025/Day02/day_02.py
@@ -5,24 +5,30 @@ print(id_ranges)
 
 id_sum = 0
 
-def is_repeating_pattern(n: int) -> bool:
-    s = str(abs(n))  # handle negatives if needed
-    if len(s) < 2:
-        return False
 
-    # Check every possible pattern length up to half the string
-    for k in range(1, len(s) // 2 + 1):
-        if len(s) % k == 0:
-            pattern = s[:k]
-            if pattern * (len(s) // k) == s:
-                print(pattern + " found in string: " + s)
-                return True
-    return False
+def is_repeating_pattern(n: int) -> bool:
+    s = str(abs(n))
+    if len(s) < 2 or len(s) % 2 != 0:
+        return False
+    half = len(s) // 2
+    return s[:half] == s[half:]
+
 
 
 def is_valid_id(id: int) -> bool:
-    return not (is_repeating_pattern(id) or str(id).startswith("0"))
+    return not (str(id).startswith("0") or is_repeating_pattern(id))
 
+
+dummy_values = [
+    '11-22',
+    '95-115',
+    '998-1012',
+    '1188511880-1188511890',
+    '222220-222224',
+    '1698522-1698528',
+    '446443-446449',
+    '38593856-38593862',
+]
 
 for id_range in id_ranges:
     range_min, range_max = id_range.split("-")
